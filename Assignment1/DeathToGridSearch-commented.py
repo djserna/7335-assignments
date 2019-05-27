42ee2196f83fe14a33002eca21dcf573498d26cd
Daniel Serna <djserna@outlook.com>
2019-05-27 13:19:46 -0500

    Added logic to create ckf accuracy dictionary and plot the accuracies.

--- Assignment1/DeathToGridSearch-commented.py
+++ Assignment1/DeathToGridSearch-commented.py
@@ -10,6 +10,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import KFold  #EDIT: I had to import KFold 
+import matplotlib.pyplot as plt
  
 # adapt this to run 
 
@@ -101,25 +102,74 @@ def runAllClfs(clfsAndHypers, data):
             print(results)
     return allResults
 
+def createAccuracyDict(results):
+    accuracyDict = {}
+    for result in results:
+        for key in result:
+            k1 = result[key]['clf']
+            v1 = result[key]['accuracy']
+            k1Test = str(k1)
+            k1Test = k1Test.replace('            ',' ') # remove large spaces from string
+            k1Test = k1Test.replace('          ',' ')
+        
+            if k1Test in accuracyDict:
+                accuracyDict[k1Test].append(v1) #append the values to create an array (techically a list) of values
+            else:
+                accuracyDict[k1Test] = [v1]
+    return accuracyDict
+
+def createPlots(accuracyDict):
+    # for determining maximum frequency (# of kfolds) for histogram y-axis
+    n = max(len(v1) for k1, v1 in accuracyDict.items())
+    
+    # for naming the plots
+    filename_prefix = 'clf_Histograms_'
+    
+    # initialize the plot_num counter for incrementing in the loop below
+    plot_num = 1 
+    
+    # Adjust matplotlib subplots for easy terminal window viewing
+    left  = 0.125  # the left side of the subplots of the figure
+    right = 0.9    # the right side of the subplots of the figure
+    bottom = 0.1   # the bottom of the subplots of the figure
+    top = 0.6      # the top of the subplots of the figure
+    wspace = 0.2   # the amount of width reserved for space between subplots,
+                   # expressed as a fraction of the average axis width
+    hspace = 0.2   # the amount of height reserved for space between subplots,
+                   # expressed as a fraction of the average axis height
+    
+    #create the histograms
+    #matplotlib is used to create the histograms: https://matplotlib.org/index.html
+    for k1, v1 in accuracyDict.items():
+        # for each key in our clfsAccuracyDict, create a new histogram with a given key's values 
+        fig = plt.figure(figsize =(10,10)) # This dictates the size of our histograms
+        ax  = fig.add_subplot(1, 1, 1) # As the ax subplot numbers increase here, the plot gets smaller
+        plt.hist(v1, facecolor='green', alpha=0.75) # create the histogram with the values
+        ax.set_title(k1, fontsize=25) # increase title fontsize for readability
+        ax.set_xlabel('Classifer Accuracy (By K-Fold)', fontsize=25) # increase x-axis label fontsize for readability
+        ax.set_ylabel('Frequency', fontsize=25) # increase y-axis label fontsize for readability
+        ax.xaxis.set_ticks(np.arange(0, 1.1, 0.1)) # The accuracy can only be from 0 to 1 (e.g. 0 or 100%)
+        ax.yaxis.set_ticks(np.arange(0, n+1, 1)) # n represents the number of k-folds
+        ax.xaxis.set_tick_params(labelsize=20) # increase x-axis tick fontsize for readability
+        ax.yaxis.set_tick_params(labelsize=20) # increase y-axis tick fontsize for readability
+        #ax.grid(True) # you can turn this on for a grid, but I think it looks messy here.
+    
+        # pass in subplot adjustments from above.
+        plt.subplots_adjust(left=left, right=right, bottom=bottom, top=top, wspace=wspace, hspace=hspace)
+        plot_num_str = str(plot_num) #convert plot number to string
+        filename = filename_prefix + plot_num_str # concatenate the filename prefix and the plot_num_str
+        plt.savefig(filename, bbox_inches = 'tight') # save the plot to the user's working directory
+        plot_num = plot_num+1 # increment the plot_num counter by 1
+    plt.show()
+
 clfs = [];
 clfs.append({RandomForestClassifier: {'n_estimators': 200, 'max_depth': 3, 'random_state': 10}})
 clfs.append({RandomForestClassifier: {'n_estimators': 250, 'max_depth': 2, 'random_state': 5}})
 clfs.append({RandomForestClassifier: {'n_estimators': 300, 'max_depth': 1, 'random_state': 7}})
 clfs.append({LogisticRegression: {'max_iter': 150, 'n_jobs': 2, 'random_state': 12}})
 allResults = runAllClfs(clfs, data)
-
-
-#original attempt using 2 dictionaries, but the clf collection seems more like a list.
-#def runAllCLFS(clfs, data):
-#    for k, v in clfs.items():
-#        for k, v in v.items():
-#            results = run(k, data, v)
-#            print(results)
-#
-#clfs = {};
-#clfs[0] = {RandomForestClassifier: {'n_estimators': 200, 'max_depth': 3, 'random_state': 10}}
-#clfs[1] = {RandomForestClassifier: {'n_estimators': 250, 'max_depth': 2, 'random_state': 5}}
-#runAllCLFS(clfs, data)
+accuracyDict = createAccuracyDict(allResults)
+createPlots(accuracyDict)
 
 #After explaining.... talk about lists and dictionaries.
 #https://docs.python.org/3/tutorial/
