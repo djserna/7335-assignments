b9101581c2fabdeb22f04d3d6bae84f697279dd5
Daniel Serna <djserna@outlook.com>
2019-07-15 19:19:37 -0500

    updated assignment 2 files.

--- Assignment2/Daniel_Serna_Assignment2.py
+++ Assignment2/Daniel_Serna_Assignment2.py
@@ -7,6 +7,13 @@ import numpy as np
 import numpy.lib.recfunctions as rfn
 from collections import OrderedDict
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+#from sklearn.linear_model import LogisticRegression
+from itertools import product
+from sklearn.model_selection import KFold  #EDIT: I had to import KFold 
 
 #NumPy Cheatsheet - https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Numpy_Python_Cheat_Sheet.pdf
 
@@ -186,3 +193,261 @@ plt.ylabel('# of Paid claims', fontsize=35)
 
 plt.title('Scatterplot of Unpaid and Paid claims by Provider', fontsize=45)
 plt.savefig('Paid_Unpaid_Scatterplot.png')
+
+#Question 3a
+percent_unpaid_jcodes = (len(Unpaid_Jcodes)/(len(Unpaid_Jcodes) + len(Paid_Jcodes))) * 100
+print(F"Question 3b: Percentage of J-codes that were unpaid: {percent_unpaid_jcodes}%")
+
+new_dtype1 = np.dtype(Unpaid_Jcodes.dtype.descr + [('IsUnpaid', '<i4')])
+new_dtype2 = np.dtype(Paid_Jcodes.dtype.descr + [('IsUnpaid', '<i4')])
+
+#create new structured array with labels
+
+#first get the right shape for each.
+Unpaid_Jcodes_w_L = np.zeros(Unpaid_Jcodes.shape, dtype=new_dtype1)
+Paid_Jcodes_w_L = np.zeros(Paid_Jcodes.shape, dtype=new_dtype2)
+
+#copy the data
+Unpaid_Jcodes_w_L['V1'] = Unpaid_Jcodes['V1']
+Unpaid_Jcodes_w_L['ClaimNumber'] = Unpaid_Jcodes['ClaimNumber']
+Unpaid_Jcodes_w_L['ClaimLineNumber'] = Unpaid_Jcodes['ClaimLineNumber']
+Unpaid_Jcodes_w_L['MemberID'] = Unpaid_Jcodes['MemberID']
+Unpaid_Jcodes_w_L['ProviderID'] = Unpaid_Jcodes['ProviderID']
+Unpaid_Jcodes_w_L['LineOfBusinessID'] = Unpaid_Jcodes['LineOfBusinessID']
+Unpaid_Jcodes_w_L['RevenueCode'] = Unpaid_Jcodes['RevenueCode']
+Unpaid_Jcodes_w_L['ServiceCode'] = Unpaid_Jcodes['ServiceCode']
+Unpaid_Jcodes_w_L['PlaceOfServiceCode'] = Unpaid_Jcodes['PlaceOfServiceCode']
+Unpaid_Jcodes_w_L['ProcedureCode'] = Unpaid_Jcodes['ProcedureCode']
+Unpaid_Jcodes_w_L['DiagnosisCode'] = Unpaid_Jcodes['DiagnosisCode']
+Unpaid_Jcodes_w_L['ClaimChargeAmount'] = Unpaid_Jcodes['ClaimChargeAmount']
+Unpaid_Jcodes_w_L['DenialReasonCode'] = Unpaid_Jcodes['DenialReasonCode']
+Unpaid_Jcodes_w_L['PriceIndex'] = Unpaid_Jcodes['PriceIndex']
+Unpaid_Jcodes_w_L['InOutOfNetwork'] = Unpaid_Jcodes['InOutOfNetwork']
+Unpaid_Jcodes_w_L['ReferenceIndex'] = Unpaid_Jcodes['ReferenceIndex']
+Unpaid_Jcodes_w_L['PricingIndex'] = Unpaid_Jcodes['PricingIndex']
+Unpaid_Jcodes_w_L['CapitationIndex'] = Unpaid_Jcodes['CapitationIndex']
+Unpaid_Jcodes_w_L['SubscriberPaymentAmount'] = Unpaid_Jcodes['SubscriberPaymentAmount']
+Unpaid_Jcodes_w_L['ProviderPaymentAmount'] = Unpaid_Jcodes['ProviderPaymentAmount']
+Unpaid_Jcodes_w_L['GroupIndex'] = Unpaid_Jcodes['GroupIndex']
+Unpaid_Jcodes_w_L['SubscriberIndex'] = Unpaid_Jcodes['SubscriberIndex']
+Unpaid_Jcodes_w_L['SubgroupIndex'] = Unpaid_Jcodes['SubgroupIndex']
+Unpaid_Jcodes_w_L['ClaimType'] = Unpaid_Jcodes['ClaimType']
+Unpaid_Jcodes_w_L['ClaimSubscriberType'] = Unpaid_Jcodes['ClaimSubscriberType']
+Unpaid_Jcodes_w_L['ClaimPrePrinceIndex'] = Unpaid_Jcodes['ClaimPrePrinceIndex']
+Unpaid_Jcodes_w_L['ClaimCurrentStatus'] = Unpaid_Jcodes['ClaimCurrentStatus']
+Unpaid_Jcodes_w_L['NetworkID'] = Unpaid_Jcodes['NetworkID']
+Unpaid_Jcodes_w_L['AgreementID'] = Unpaid_Jcodes['AgreementID']
+
+#And assign the target label 
+Unpaid_Jcodes_w_L['IsUnpaid'] = 1
+
+# Do the same for the Paid set.
+
+#copy the data
+Paid_Jcodes_w_L['V1'] = Paid_Jcodes['V1']
+Paid_Jcodes_w_L['ClaimNumber'] = Paid_Jcodes['ClaimNumber']
+Paid_Jcodes_w_L['ClaimLineNumber'] = Paid_Jcodes['ClaimLineNumber']
+Paid_Jcodes_w_L['MemberID'] = Paid_Jcodes['MemberID']
+Paid_Jcodes_w_L['ProviderID'] = Paid_Jcodes['ProviderID']
+Paid_Jcodes_w_L['LineOfBusinessID'] = Paid_Jcodes['LineOfBusinessID']
+Paid_Jcodes_w_L['RevenueCode'] = Paid_Jcodes['RevenueCode']
+Paid_Jcodes_w_L['ServiceCode'] = Paid_Jcodes['ServiceCode']
+Paid_Jcodes_w_L['PlaceOfServiceCode'] = Paid_Jcodes['PlaceOfServiceCode']
+Paid_Jcodes_w_L['ProcedureCode'] = Paid_Jcodes['ProcedureCode']
+Paid_Jcodes_w_L['DiagnosisCode'] = Paid_Jcodes['DiagnosisCode']
+Paid_Jcodes_w_L['ClaimChargeAmount'] = Paid_Jcodes['ClaimChargeAmount']
+Paid_Jcodes_w_L['DenialReasonCode'] = Paid_Jcodes['DenialReasonCode']
+Paid_Jcodes_w_L['PriceIndex'] = Paid_Jcodes['PriceIndex']
+Paid_Jcodes_w_L['InOutOfNetwork'] = Paid_Jcodes['InOutOfNetwork']
+Paid_Jcodes_w_L['ReferenceIndex'] = Paid_Jcodes['ReferenceIndex']
+Paid_Jcodes_w_L['PricingIndex'] = Paid_Jcodes['PricingIndex']
+Paid_Jcodes_w_L['CapitationIndex'] = Paid_Jcodes['CapitationIndex']
+Paid_Jcodes_w_L['SubscriberPaymentAmount'] = Paid_Jcodes['SubscriberPaymentAmount']
+Paid_Jcodes_w_L['ProviderPaymentAmount'] = Paid_Jcodes['ProviderPaymentAmount']
+Paid_Jcodes_w_L['GroupIndex'] = Paid_Jcodes['GroupIndex']
+Paid_Jcodes_w_L['SubscriberIndex'] = Paid_Jcodes['SubscriberIndex']
+Paid_Jcodes_w_L['SubgroupIndex'] = Paid_Jcodes['SubgroupIndex']
+Paid_Jcodes_w_L['ClaimType'] = Paid_Jcodes['ClaimType']
+Paid_Jcodes_w_L['ClaimSubscriberType'] = Paid_Jcodes['ClaimSubscriberType']
+Paid_Jcodes_w_L['ClaimPrePrinceIndex'] = Paid_Jcodes['ClaimPrePrinceIndex']
+Paid_Jcodes_w_L['ClaimCurrentStatus'] = Paid_Jcodes['ClaimCurrentStatus']
+Paid_Jcodes_w_L['NetworkID'] = Paid_Jcodes['NetworkID']
+Paid_Jcodes_w_L['AgreementID'] = Paid_Jcodes['AgreementID']
+
+#And assign the target label 
+Paid_Jcodes_w_L['IsUnpaid'] = 0
+
+Jcodes_w_L = np.concatenate((Unpaid_Jcodes_w_L, Paid_Jcodes_w_L), axis=0)
+np.random.shuffle(Jcodes_w_L)
+
+Jcodes_w_L.dtype.names
+
+label =  'IsUnpaid'
+
+cat_features = ['ProviderID','LineOfBusinessID','RevenueCode', 
+                'ServiceCode', 'PlaceOfServiceCode', 'ProcedureCode',
+                'DenialReasonCode','PriceIndex', 'InOutOfNetwork', 'ReferenceIndex', 
+                'PricingIndex', 'CapitationIndex', 'ClaimSubscriberType',
+                'ClaimPrePrinceIndex', 'ClaimCurrentStatus', 'NetworkID',
+                'AgreementID', 'ClaimType']
+
+numeric_features = ['ClaimNumber', 'ClaimLineNumber', 'MemberID', 
+                    'ClaimChargeAmount',
+                    'SubscriberPaymentAmount', 'ProviderPaymentAmount',
+                    'GroupIndex', 'SubscriberIndex', 'SubgroupIndex']
+
+Mcat = np.array(Jcodes_w_L[cat_features].tolist())
+Mnum = np.array(Jcodes_w_L[numeric_features].tolist())
+
+L = np.array(Jcodes_w_L[label].tolist())
+
+# Run the OneHotEncoder
+# You can encounter a memory error here in which case, you probably should subset.
+ohe = OneHotEncoder(sparse=False) #Easier to read
+Mcat = ohe.fit_transform(Mcat)
+
+#Concatenate the columns
+M = np.concatenate((Mcat, Mnum), axis=1)
+#M = np.concatenate((Mcat_subset, Mnum_subset), axis=1)
+
+
+L = Jcodes_w_L[label].astype(int)
+
+n_folds = 5
+
+#EDIT: pack the arrays together into "data"
+data = (M,L,n_folds)
+
+
+
+
+
+#EDIT: A function, "run", to run all our classifiers against our data.
+
+def run(a_clf, data, clf_hyper={}):
+  M, L, n_folds = data #EDIT: unpack the "data" container of arrays
+  kf = KFold(n_splits=n_folds) # JS: Establish the cross validation 
+  ret = {} # JS: classic explicaiton of results
+  
+  for ids, (train_index, test_index) in enumerate(kf.split(M, L)): #EDIT: We're interating through train and test indexes by using kf.split
+                                                                   #      from M and L.
+                                                                   #      We're simply splitting rows into train and test rows
+                                                                   #      for our five folds.
+    
+    clf = a_clf(**clf_hyper) # JS: unpack paramters into clf if they exist   #EDIT: this gives all keyword arguments except 
+                                                                             #      for those corresponding to a formal parameter
+                                                                             #      in a dictionary.
+            
+    clf.fit(M[train_index], L[train_index])   #EDIT: First param, M when subset by "train_index", 
+                                              #      includes training X's. 
+                                              #      Second param, L when subset by "train_index",
+                                              #      includes training Y.                             
+    
+    pred = clf.predict(M[test_index])         #EDIT: Using M -our X's- subset by the test_indexes, 
+                                              #      predict the Y's for the test rows.
+    
+    ret[ids]= {'clf': clf,                    #EDIT: Create arrays of
+               'train_index': train_index,
+               'test_index': test_index,
+               'accuracy': accuracy_score(L[test_index], pred)}    
+  return ret
+
+
+
+
+def populateClfAccuracyDict(results):
+    for key in results:
+        k1 = results[key]['clf'] 
+        v1 = results[key]['accuracy']
+        k1Test = str(k1) #Since we have a number of k-folds for each classifier...
+                         #We want to prevent unique k1 values due to different "key" values
+                         #when we actually have the same classifer and hyper parameter settings.
+                         #So, we convert to a string
+                        
+        #String formatting            
+        k1Test = k1Test.replace('            ',' ') # remove large spaces from string
+        k1Test = k1Test.replace('          ',' ')
+        
+        #Then check if the string value 'k1Test' exists as a key in the dictionary
+        if k1Test in clfsAccuracyDict:
+            clfsAccuracyDict[k1Test].append(v1) #append the values to create an array (techically a list) of values
+        else:
+            clfsAccuracyDict[k1Test] = [v1] #create a new key (k1Test) in clfsAccuracyDict with a new value, (v1)            
+        
+            
+
+def myHyperSetSearch(clfsList,clfDict):
+    #hyperSet = {}
+    for clf in clfsList:
+    
+    #I need to check if values in clfsList are in clfDict
+        clfString = str(clf)
+        #print("clf: ", clfString)
+        
+        for k1, v1 in clfDict.items(): # go through the inner dictionary of hyper parameters
+            #Nothing to do here, we need to get into the inner nested dictionary.
+            if k1 in clfString:
+                #allows you to do all the matching key and values
+                k2,v2 = zip(*v1.items()) # explain zip (https://docs.python.org/3.3/library/functions.html#zip)
+                for values in product(*v2): #for the values in the inner dictionary, get their unique combinations from product()
+                    hyperSet = dict(zip(k2, values)) # create a dictionary from their values
+                    results = run(clf, data, hyperSet) # pass the clf and dictionary of hyper param combinations to run; get results
+                    populateClfAccuracyDict(results) # populate clfsAccuracyDict with results
+ 
+
+
+
+clfsList = [RandomForestClassifier, LogisticRegression] 
+
+clfDict = {'RandomForestClassifier': {"min_samples_split": [2,3,4], 
+                                      "n_jobs": [1,2,3]},'LogisticRegression': {"tol": [0.001,0.01,0.1]}}
+
+                   
+#Declare empty clfs Accuracy Dict to populate in myHyperSetSearch     
+clfsAccuracyDict = {}
+
+#Run myHyperSetSearch
+myHyperSetSearch(clfsList,clfDict)
+
+n = max(len(v1) for k1, v1 in clfsAccuracyDict.items())
+
+# for naming the plots
+filename_prefix = 'clf_Histograms_'
+
+# initialize the plot_num counter for incrementing in the loop below
+plot_num = 1 
+
+# Adjust matplotlib subplots for easy terminal window viewing
+left  = 0.125  # the left side of the subplots of the figure
+right = 0.9    # the right side of the subplots of the figure
+bottom = 0.1   # the bottom of the subplots of the figure
+top = 0.6      # the top of the subplots of the figure
+wspace = 0.2   # the amount of width reserved for space between subplots,
+               # expressed as a fraction of the average axis width
+hspace = 0.2   # the amount of height reserved for space between subplots,
+               # expressed as a fraction of the average axis height
+               
+
+
+#create the histograms
+for k1, v1 in clfsAccuracyDict.items():
+    # for each key in our clfsAccuracyDict, create a new histogram with a given key's values 
+    fig = plt.figure(figsize =(10,10)) # This dictates the size of our histograms
+    ax  = fig.add_subplot(1, 1, 1) # As the ax subplot numbers increase here, the plot gets smaller
+    plt.hist(v1, facecolor='green', alpha=0.75) # create the histogram with the values
+    ax.set_title(k1, fontsize=25) # increase title fontsize for readability
+    ax.set_xlabel('Classifer Accuracy (By K-Fold)', fontsize=25) # increase x-axis label fontsize for readability
+    ax.set_ylabel('Frequency', fontsize=25) # increase y-axis label fontsize for readability
+    ax.xaxis.set_ticks(np.arange(0, 1.1, 0.1)) # The accuracy can only be from 0 to 1 (e.g. 0 or 100%)
+    ax.yaxis.set_ticks(np.arange(0, n+1, 1)) # n represents the number of k-folds
+    ax.xaxis.set_tick_params(labelsize=20) # increase x-axis tick fontsize for readability
+    ax.yaxis.set_tick_params(labelsize=20) # increase y-axis tick fontsize for readability
+    #ax.grid(True) # you can turn this on for a grid, but I think it looks messy here.
+
+    # pass in subplot adjustments from above.
+    plt.subplots_adjust(left=left, right=right, bottom=bottom, top=top, wspace=wspace, hspace=hspace)
+    plot_num_str = str(plot_num) #convert plot number to string
+    filename = filename_prefix + plot_num_str # concatenate the filename prefix and the plot_num_str
+    plt.savefig(filename, bbox_inches = 'tight') # save the plot to the user's working directory
+    plot_num = plot_num+1 # increment the plot_num counter by 1
+plt.show()
